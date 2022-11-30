39206aadccb8557fee3642fe5ccd394788a2e992
Gent Mulaj <mulajgent2001@gmail.com>
2022-11-30 16:37:29 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,83 @@
+function switchVisible1() {
+    if (document.getElementById('Div1')) {
+
+        if (document.getElementById('Div1').style.display == 'none') {
+            document.getElementById('Div1').style.display = 'block';
+            document.getElementById('Div2').style.display = 'none';
+        }
+        else {
+            document.getElementById('Div1').style.display = 'none';
+            document.getElementById('Div2').style.display = 'block';
+        }
+    }
+}
+
+function switchVisible2() {
+    if (document.getElementById('Div3')) {
+
+        if (document.getElementById('Div3').style.display == 'none') {
+            document.getElementById('Div3').style.display = 'block';
+            document.getElementById('Div4').style.display = 'none';
+        }
+        else {
+            document.getElementById('Div3').style.display = 'none';
+            document.getElementById('Div4').style.display = 'block';
+        }
+    }
+}
+
+function switchVisible3() {
+    if (document.getElementById('Div5')) {
+
+        if (document.getElementById('Div5').style.display == 'none') {
+            document.getElementById('Div5').style.display = 'block';
+            document.getElementById('Div6').style.display = 'none';
+        }
+        else {
+            document.getElementById('Div5').style.display = 'none';
+            document.getElementById('Div6').style.display = 'block';
+        }
+    }
+}
+
+function switchVisible4() {
+    if (document.getElementById('Div7')) {
+
+        if (document.getElementById('Div7').style.display == 'none') {
+            document.getElementById('Div7').style.display = 'block';
+            document.getElementById('Div8').style.display = 'none';
+        }
+        else {
+            document.getElementById('Div7').style.display = 'none';
+            document.getElementById('Div8').style.display = 'block';
+        }
+    }
+}
+
+function switchVisible5() {
+    if (document.getElementById('Div9')) {
+
+        if (document.getElementById('Div9').style.display == 'none') {
+            document.getElementById('Div9').style.display = 'block';
+            document.getElementById('Div10').style.display = 'none';
+        }
+        else {
+            document.getElementById('Div9').style.display = 'none';
+            document.getElementById('Div10').style.display = 'block';
+        }
+    }
+}
+
+function switchVisible6() {
+    if (document.getElementById('Div11')) {
+
+        if (document.getElementById('Div11').style.display == 'none') {
+            document.getElementById('Div11').style.display = 'block';
+            document.getElementById('Div12').style.display = 'none';
+        }
+        else {
+            document.getElementById('Div11').style.display = 'none';
+            document.getElementById('Div12').style.display = 'block';
+        }
+    }
+}
